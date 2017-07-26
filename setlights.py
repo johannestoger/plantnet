713c518446b36fe90d096bbd9c704e461cf27d4f
Johannes Toger <johannes.toger@gmail.com>
2017-07-26 10:37:38 +0200

    automated on/off based on time

--- setlights.py
+++ setlights.py
@@ -1,8 +1,14 @@
+#!/usr/bin/env python3
 """Provide a CLI for Tradfri."""
 import logging
 from pprint import pprint
 import sys
+import os
+import datetime
+import time
 
+here_path = os.path.dirname(os.path.realpath(__file__))
+os.chdir(here_path)
 sys.path.append('pytradfri')
 
 from pytradfri.const import *  # noqa
@@ -17,7 +23,7 @@ def main():
     with open('gateway.key', 'r') as keyfile:
         gateway_key = keyfile.read().replace('\n','')
 
-    logging.basicConfig(level=logging.DEBUG)
+    logging.basicConfig(filename='plantnet.log', level=logging.INFO)
 
     api = api_factory(host, gateway_key)
     gateway = Gateway()
@@ -31,9 +37,26 @@ def main():
     mood = moods[0]
     tasks = api(gateway.get_smart_tasks())
 
+    # Compute light level for current time
+    tz = datetime.datetime.now(datetime.timezone.utc).astimezone().tzinfo
+    now = datetime.datetime.now(tz)
+
+    starttime = now.replace(hour=10, minute=28, second=0, microsecond=0)
+    endtime = now.replace(hour=10, minute=35, second=0, microsecond=0)
+
+    light_on = 10
+    light_off = 0
+    light_setlevel = light_off
+
+    if now >= starttime and now <= endtime:
+        light_setlevel = light_on
+
+    # Set lights
+    logstr = 'plantnet {}: Setting lights to {}'.format(time.strftime('%c'), light_setlevel)
+    logging.info(logstr)
 
     for light in lights:
-        api(light.light_control.set_dimmer(int(sys.argv[1])))
+        api(light.light_control.set_dimmer(light_setlevel))
 
 #print()
 #print("Example commands:")
